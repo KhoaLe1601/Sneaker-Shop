3a350374c35a334661be4b4345599944cf9c40b2
Khoa Le <leminhkhoabl123@gmail.com>
2023-11-16 22:51:01 +0700

    khoi tao giao dien

--- src/services/sneaker.service.js
+++ src/services/sneaker.service.js
@@ -1,8 +1,22 @@
-import createApiClient from "./api.service"
+import createApiClient from './api.service'
 
 //lay du lieu tu server
 class ShopService {
-    constructor(baseUrl = "/api/sneakershop") {
+    constructor(baseUrl = "/sneakershop/auth") {
         this.api = createApiClient(baseUrl);
     }
+
+    async register(data) {
+        return (await this.api.post("/register")).data;
+    }
+
+    async login() {
+        return (await this.api.post("/login")).data;
+    }
+
+    async logout() {
+        return (await this.api.post("/logout")).data
+    }
 }
+
+export default new ShopService();
